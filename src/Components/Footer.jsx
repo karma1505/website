ca4f8a2b3e81b147d7ce1669a18f47a44f566fe0
Karmanya <karmanyasingh8@gmail.com>
2024-06-25 16:37:33 +0530

    working update 1.5

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
-import "../App.css";
+import '../App.css'; // Import your CSS file for styling
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import instalogo from '../Media/instalogo.svg';
 
 const Footer = () => {
   return (
-    <div className="page-container">
-      <footer className="footer">
-        <div className="footer-container">
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="left-content">
+          <p className="social-para">
+            Follow Us 
+          </p>
+          <a href="https://instagram.com/dillihouse_dharampur/" target="_blank" rel="noopener noreferrer">
+              <img src={instalogo} alt="instalogo" className="instalogo" />
+          </a>
+        </div>
+        <div className="center-content">
           <p>&copy; 2024 Dilli House. All rights reserved.</p>
           <p><FontAwesomeIcon icon={faPhone} /> : +91 12345 67890</p>
           <p><FontAwesomeIcon icon={faEnvelope} /> : <a href="mailto:info@dillihouse.com">info@dillihouse.com</a></p>
           <p>Address: Dharampur, Kasauli, Himachal Pradesh, India</p>
-          <p className="social-para">Follow Us <a href="https://instagram.com/dillihouse_dharampur/" target="_blank" rel="noopener noreferrer"><img src={instalogo} alt="instalogo" className="instalogo" /></a></p>
         </div>
-      </footer>
-    </div>
+      </div>
+    </footer>
   );
 };
 
