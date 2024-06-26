b2cb71fb23d6d17bd5c7e908d862592e272d900c
Karmanya <karmanyasingh8@gmail.com>
2024-06-26 21:34:07 +0530

    footer complete

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,6 +3,8 @@ import "../App.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import instalogo from '../Media/instalogo.svg';
+import facebooklogo from '../Media/facebooklogo.svg';
+
 
 const Footer = () => {
   return (
@@ -17,14 +19,14 @@ const Footer = () => {
               <img src={instalogo} alt="Instagram" className="social-logo" />
             </a>
             <a href="https://instagram.com/dillihouse_dharampur/" target="_blank" rel="noopener noreferrer">
-              <img src={instalogo} alt="Facebook" className="social-logo" />
+              <img src={facebooklogo} alt="Facebook" className="social-logo" />
             </a>
           </div>
         </div>
         <div className="center-content">
           <p>&copy; 2024 Dilli House. All rights reserved.</p>
           <p><FontAwesomeIcon icon={faPhone} /> : +91 12345 67890</p>
-          <p><FontAwesomeIcon icon={faEnvelope} /> : <a href="mailto:info@dillihouse.com">info@dillihouse.com</a></p>
+          <p><FontAwesomeIcon icon={faEnvelope} /> : <a href="mailto:info@dillihouse.com" className='mail'>info@dillihouse.com</a></p>
           <p>Address: Dharampur, Kasauli, Himachal Pradesh, India</p>
         </div>
       </div>
