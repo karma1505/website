0dd52498f27a93a9bbdb7f180efaae5e509059ee
Karmanya <karmanyasingh8@gmail.com>
2024-07-02 21:34:58 +0530

    footer booking added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,11 +4,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import instalogo from '../Media/instalogo.svg';
 import facebooklogo from '../Media/facebooklogo.svg';
-
+import makemytriplogo from '../Media/mmtlogo.svg';
 
 const Footer = () => {
   return (
-    <footer className="footer" style={{ backgroundColor: '#706C61', color:'white' }}>
+    <footer className="footer">
       <div className="footer-container">
         <div className="left-content">
           <p className="social-para">
@@ -23,11 +23,23 @@ const Footer = () => {
             </a>
           </div>
         </div>
+
         <div className="center-content">
           <p>&copy; 2024 Dilli House. All rights reserved.</p>
           <p><FontAwesomeIcon icon={faPhone} /> : +91 12345 67890</p>
           <p><FontAwesomeIcon icon={faEnvelope} /> : <a href="mailto:info@dillihouse.com" className='mail'>info@dillihouse.com</a></p>
           <p>Address: Dharampur, Kasauli, Himachal Pradesh, India</p>
+        </div>
+
+        <div className="right-content">
+          <p className="book-stay-para">
+            Book Your Stay
+          </p>
+          <div className="book-stay">
+            <a href="https://www.makemytrip.com/hotels/dilli_villa_3bhk_swimming_pool_jacuzzi_and_home_theatre-details-kasauli.html" target="_blank" rel="noopener noreferrer">
+              <img src={makemytriplogo} alt="MakeMyTrip" className="makemytrip-logo" />
+            </a>
+          </div>
         </div>
       </div>
     </footer>
